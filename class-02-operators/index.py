993ac9d91abec233fac81732d40e10d924f3d232
NathanLins12 <nathan.lins012@gmail.com>
2024-04-26 19:39:16 -0300

    add class operators

---
+++ class-02-operators/index.py
@@ -0,0 +1,64 @@
+import math
+
+num1 = 4
+num2 = 6
+
+# Arithmeric
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** num2)
+print(num1 % num2)
+
+print(pow(num1, num2))
+print(round(1.5))
+print(math.ceil(1.1))
+print(math.floor(1.9))
+
+# Assignment
+
+num = 2
+num = num + 1
+
+num = 2
+num = num + 1
+num += 1
+num -= 1
+num *= 1
+num /= 1
+num **= 1
+num %= 1
+
+print(num)
+
+# Comparison
+
+print(2 == "2")
+print(2 == 2)
+print(2 == 3)
+print(2 != 3)
+print(2 != 2)
+print(2 > 3)
+print(2 < 3)
+print(2 <= 3)
+print(2 >= 3)
+
+# Logical
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(2 == 2 and 3 == 3)
+print(2 != 2 and 3 == 3)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print(2 != 2 or not 3 == 3)
+
+print(not True)
+print(not False)
